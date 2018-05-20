abd77a36780201ab05f4c5729886f2ec38c12a5e
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-05-19 20:16:02 -0500

    Added functionality for authenication

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
+const config = require('../config/database');
 const User = require('../models/users');
 
 //registrate
@@ -25,7 +25,37 @@ router.post('/register', (req, res, next) => {
 
 //authenticate
 router.post('/authenticate', (req, res, next) => {
-  res.send('authenticate');
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return res.json({success: false, msg: 'User not found'});
+    }
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign(user.toJSON(), config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({success: false, msg: 'Incorrect password'});
+      }
+    });
+
+  });
 });
 
 //profile
