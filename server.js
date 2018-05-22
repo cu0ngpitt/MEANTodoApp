327170eec70803ae747491b22309f0740eaff205
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-05-22 13:52:16 -0500

    Added notes to my server for personal learning and memory

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-//set up ==========================
+//create our app server with express
 const express = require('express');
-const path = require('path');                 //installs with cors module so it is not included in the dependencies listen
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const config = require('./config/database');
-
 const app = express();
 
-const users = require('./routes/users');
+//set up ==========================
+const path = require('path');                 //installs with cors module so it is not included in the dependencies listen
+const bodyParser = require('body-parser');    //pull information from HTML POST
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');         //mongoose for mongodb
+const config = require('./config/database');
 
-const port = 3000;
+
+const users = require('./routes/users');      //loads our custom router module/middleware to our app
+app.use('/users', users);                     //mounts our router with a base address of localhost:"our port #"/users
+
+const port = 3000;                            //sets our local port to 3000
 
 mongoose.connect(config.database);
 mongoose.connection.on('connected', () => {
@@ -30,13 +33,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
 
-app.use('/users', users);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));      // set the static files location
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+//listen for connections on our designated port (start app with node server.js) ===========
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
