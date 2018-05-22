ce5380bef2fd4eafd1ba2cf4b6798acdb6dbde92
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-05-21 21:34:37 -0500

    Added form validation functionality

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ValidateService } from '../../services/validate.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  firstName: String;
+  lastName: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Required Fields
+    if(!this.validateService.validateRegistration(user)) {
+      console.log("Please complete the required fields");
+      return false;
+    }
+
+    // Validate Email
+    if(!this.validateService.validateEmail(user.email)) {
+      console.log("Please enter a valid email");
+      return false;
+    }
   }
 
 }
