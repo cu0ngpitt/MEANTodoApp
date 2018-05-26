dd2d17458c0828bd425ffc02ae2d966c3409575f
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-05-26 16:46:20 -0500

    Added angular2-jwt middleware to allow protected request functionality to the nav bar

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';             //middleware installed: angular2-jwt
 
 @Injectable()
 export class AuthService {
@@ -23,11 +24,29 @@ export class AuthService {
       .map(res => res.json());
   }
 
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', { headers: headers })
+      .map(res => res.json());
+  }
+
   storeUserData(token, user) {
-    localStorage.setItem('id_token', token);
+    localStorage.setItem('token', token);                             //must set the token variable to 'token' in order to use the angular2-jwt middleware function tokenNotExpired()
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired();                       //angular2-jwt assumes that the standard name of the token is token, therefore we must set the token name to 'token' (see the storeUserData function)
   }
 
   logout() {
