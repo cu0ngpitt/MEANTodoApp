afa3167930929b2b5091553662517ea1b20d7e4b
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-11 14:42:54 -0500

    Added functionality to the to do list. Can now get the list, add to the list, and delete from the list

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ListService } from '../../services/list.service';
+//import { List } from '../../list';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  user: Object;
 
-  constructor() { }
+  lists: any;
+
+  @Input() input;
+
+  constructor(
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private listService: ListService
+  ) { }
 
   ngOnInit() {
+    this.getLists();
+
+    this.authService.getProfile().subscribe(profile => {
+      this.user = profile.user;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
+
+  getLists(): void {
+    this.listService.getAllTodos()
+      .subscribe((data: any) => this.lists = data.todos);
+  }
+
+  addList(newItem): void {
+    const newTodo = { item: newItem, completed: false };
+
+    if (newItem !== '')
+      this.listService.addItem(newTodo).subscribe(data => {
+          this.lists.push(data);
+          this.getLists();
+      });
+    console.log(this.lists);
+  }
+
+  isChecked(list) {
+    if(list.completed === false) {
+      this.listService.markCompleted(list).subscribe((data: any) => {
+        this.lists = data.todos;
+        this.getLists();
+        });
+    } else if (list.completed === true) {
+      this.listService.markNotCompleted(list).subscribe((data: any) => {
+        this.lists = data.todos;
+        this.getLists();
+      });
+    }
+  }
+
+  delList(list) {
+    this.listService.deleteCompleted(list).subscribe((data: any) => {
+      this.lists = data.todos;
+      this.getLists();
+    });
+    }
 
 }
